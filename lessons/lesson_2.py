1971d7d0752493aeda33287a2ed3efd64782bdeb
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2022-05-17 20:09:40 +0300

    Update lesson_2.py

--- lessons/lesson_2.py
+++ lessons/lesson_2.py
@@ -12,3 +12,25 @@ class Animal:
 
 animal = Animal('Cat')
 print(animal.animal_name)
+
+
+class Animal:
+    voice = 'some'
+    _voice = 'some'
+    __voice = 'voice'
+
+    def get_voice(self):
+        print(self.voice)
+        print(self._voice)
+        print(self.__voice)
+
+
+class Cat(Animal):
+    def say_hello(self):
+        print(self.voice)
+        print(self._voice)
+        print(self._Animal__voice)
+
+
+animal = Cat()
+animal.say_hello()
