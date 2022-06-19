f6c36df593271ae7a90f8e758785f464476594eb
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2022-06-19 15:06:02 +0300

    lesson_10

---
+++ lessons/test_lesson_10.py
@@ -0,0 +1,36 @@
+"""
+Продолжаем знакомиться с Requests:
+ - post, delete, patch, put запросы
+ - Отличие data, от json
+ - Работа с query params
+ - Работа с headers
+"""
+import requests
+
+body = {
+  "title": "New lesson"
+}
+
+url = 'http://46.101.117.86/api/v1'
+response_post = requests.post(url + '/lessons/', json=body)
+
+# print(response_post.request.headers)
+# print(response_post.status_code)
+# print(response_post.json())
+
+response_delete = requests.delete(url + '/lessons/', json={})
+# print(response_post.status_code)
+# print(response_post.json())
+
+response_patch = requests.patch(url + '/lessons/', json=body)
+
+response_put = requests.put(url + '/lessons/', json=body)
+
+query_params = {
+    'x': 5,
+    'y': 6
+}
+
+response_get = requests.get(url + '/lessons/', params=query_params)
+print(response_get.status_code)
+print(response_get.request.url)
---
+++ lessons/test_lesson_10_1.py
@@ -0,0 +1,38 @@
+
+import http
+from attr import field
+import requests
+
+token = "a66208bce6ac0a02eb4849b8eecbd1bc5856f31e"
+url = 'http://46.101.117.86/api/v1/courses/'
+
+headers = {
+    'Authorization': f'Token {token}'
+}
+
+response = requests.get(url, headers=headers)
+
+# print(response.status_code)
+# print(response.request.headers)
+
+# assert response.status_code == http.HTTPStatus.OK, f'Expected code 200, but get {response.status_code}'
+
+send_file_url = 'http://46.101.117.86/api/v1/courses/send-file/'
+
+with open('hello.json', 'r') as file:
+   response = requests.post(send_file_url, headers=headers, files={'file': file})
+   print(response.status_code)
+   print(response.json())
+   print(response.request.headers)
+
+
+our_file = open('hello.json', 'w')
+our_file.write('{"hello": "some_file"}')
+our_file.close()
+
+read_file = open('hello.json', 'r')
+# print(read_file.readline(), type(read_file.readline()))
+read_file.close()
+
+with open('hello.json', 'w') as file:
+    file.write('{"hello": "some_file"}')
